805120d470a183f4844f6eb9394c31d7021a3b33
masahiro <wktkmrnhrtmmshr@gmail.com>
2023-02-11 01:57:27 +0900

    並列処理の実装

--- webserver.py
+++ webserver.py
@@ -1,48 +1,35 @@
 import socket
-from datetime import datetime
 
+from workerthread import WorkerThread
 
-class TCPServer:
-
+class WebServer:
   def serve(self):
-    print("=== サーバーを起動します ===")
+    print("=== Server: サーバーを起動します ===")
 
     try:
-      server_socket = socket.socket()
-      server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      server_socket = self.create_server_socket()
 
-      server_socket.bind(("localhost", 8080))
-      server_socket.listen(10)
+      while True:
 
-      print("=== クライアントからの接続を待ちます ===")
-      (client_socket, address) = server_socket.accept()
-      print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+        print("=== Server: クライアントからの接続を待ちます ===")
+        (client_socket, address) = server_socket.accept()
+        print(f"=== Server: クライアントとの接続が完了しました remote_address: {address} ===")
 
-      request = client_socket.recv(4096)
+        thread = WorkerThread(client_socket, address)
 
-      with open("server_recv.txt", "wb") as f:
-        f.write(request)
-
-      response_body = "<html><body><h1>It works!</h1></body></html>"
-
-      response_line = "HTTP/1.1 200 OK\r\n"
-
-      response_header = ""
-      response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-      response_header += "Host: HenaServer/0.1\r\n"
-      response_header += f"Content-Length: {len(response_body.encode())}\r\n"
-      response_header += "Connection: Close\r\n"
-      response_header += "Content-Type: text/html\r\n"
-
-      response = (response_line + response_header + "\r\n" + response_body).encode()
-
-      client_socket.send(response)
-
-      client_socket.close()
-
+        thread.start()
     finally:
-      print("=== サーバーを停止します。 ===")
+      print("=== Server: サーバーを停止します。 ===")
+
+  def create_server_socket(self) -> socket:
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind(("localhost", 8080))
+    server_socket.listen(10)
+
+    return server_socket
 
 if __name__ == '__main__':
-  server = TCPServer()
+  server = WebServer()
   server.serve()
