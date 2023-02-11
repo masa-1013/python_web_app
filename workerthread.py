8f4df08ef5730b5a56bcc59fcc28f26ae72246ad
masahiro <wktkmrnhrtmmshr@gmail.com>
2023-02-11 12:30:00 +0900

    動的なhtmlを返す

--- workerthread.py
+++ workerthread.py
@@ -1,9 +1,12 @@
 import os
+import re
+import textwrap
 import traceback
 from datetime import datetime
+from pprint import pformat
 from socket import socket
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, Optional
 
 class WorkerThread(Thread):
   BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -32,15 +35,56 @@ class WorkerThread(Thread):
 
       method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-      try:
-        response_body = self.get_static_file_content(path)
+      response_body: bytes
+      content_type: Optional[str]
+      response_line: str
+
+      if path == "/now":
+        html = f"""\
+          <html>
+          <body>
+              <h1>Now: {datetime.now()}</h1>
+          </body>
+          </html>
+        """
+        response_body = textwrap.dedent(html).encode()
+
+        content_type = "text/html"
 
         response_line = "HTTP/1.1 200 OK\r\n"
-      except OSError:
-        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-        response_line = "HTTP/1.1 404 Not Found\r\n"
+      elif path == "/show_request":
+        html = f"""\
+          <html>
+          <body>
+              <h1>Request Line:</h1>
+              <p>
+                  {method} {path} {http_version}
+              </p>
+              <h1>Headers:</h1>
+              <pre>{pformat(request_header)}</pre>
+              <h1>Body:</h1>
+              <pre>{request_body.decode("utf-8", "ignore")}</pre>
+              
+          </body>
+          </html>
+        """
+        response_body = textwrap.dedent(html).encode()
 
-      response_header = self.build_response_header(path, response_body)
+        content_type = "text/html"
+
+        response_line = "HTTP/1.1 200 OK\r\n"
+      else:
+        try:
+          response_body = self.get_static_file_content(path)
+
+          content_type = None
+
+          response_line = "HTTP/1.1 200 OK\r\n"
+        except OSError:
+          response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+          response_line = "HTTP/1.1 404 Not Found\r\n"
+
+      response_header = self.build_response_header(path, response_body, content_type)
 
       response = (response_line + response_header + "\r\n").encode() + response_body
 
@@ -52,13 +96,19 @@ class WorkerThread(Thread):
       print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
       self.client_socket.close()
 
-  def parse_http_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
+  def parse_http_request(self, request: bytes) -> Tuple[str, str, str, dict, bytes]:
     request_line, remain = request.split(b"\r\n", maxsplit=1)
     request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
 
     method, path, http_version = request_line.decode().split(" ")
 
-    return method, path, http_version, request_header, request_body
+    headers = {}
+
+    for header_row in request_header.decode().split("\r\n"):
+      key, value = re.split(r": *", header_row, maxsplit=1)
+      headers[key] = value
+
+    return method, path, http_version, headers, request_body
 
   def get_static_file_content(self, path: str) -> bytes:
     relative_path = path.lstrip("/")
@@ -66,13 +116,14 @@ class WorkerThread(Thread):
     with open(stacic_file_path, "rb") as f:
       return f.read()
 
-  def build_response_header(self, path: str, response_body: bytes) -> str:
-    if "." in path:
-      ext = path.rsplit(".", maxsplit=1)[-1]
-    else:
-      ext = ""
+  def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
+    if content_type is None:
+      if "." in path:
+        ext = path.rsplit(".", maxsplit=1)[-1]
+      else:
+        ext = ""
 
-    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+      content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
 
     response_header = ""
     response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
